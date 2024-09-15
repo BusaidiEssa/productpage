3ab38b104b539664b8b32caccb1ad6e975f2f594
BusaidiEssa <essas.work@gmail.com>
2024-09-15 07:56:39 +0400

    Installed react and its dependencies

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import productRoutes from "./routes/product.route.js"
 dotenv.config();
 
 const app = express();
+const PORT= process.env.PORT || 5000;
 
 app.use(express.json())
 app.use("/api/products",productRoutes)
@@ -17,6 +18,6 @@ app.use("/api/products",productRoutes)
 
 app.listen(5000,()=>{
     connectDB();
-    console.log('Server started at http://localhost:5000')
+    console.log('Server started at http://localhost:'+PORT)
 })
 
