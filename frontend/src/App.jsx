b58d56b989d1f79af69529e94474d8e3e636755b
BusaidiEssa <essas.work@gmail.com>
2024-09-21 10:10:15 +0400

    Finished navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,19 @@
 import { Box, Button } from "@chakra-ui/react"
 import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/HomePage"
-import CreatPage from "./pages/CreatePage"
+import HomePage from "./pages/Homepage.jsx"
+import CreatePage from "./pages/Createpage.jsx"
 import Navbar from "./components/Navbar"
+import { useColorModeValue } from "@chakra-ui/color-mode"
 
 function App() {
 
   return (
     <>
-    <Box minH={"100vh"}>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")} > 
       <Navbar/>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/create" element={<CreatPage />} />
+        <Route path="/create" element={<CreatePage />} />
       </Routes>
     </Box>
     </>
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Createpage = () => {
   return (
     <div>
-      CreatePage
+      .
     </div>
   )
 }
