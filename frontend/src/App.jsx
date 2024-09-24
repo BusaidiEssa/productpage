e4b350e0984205d9aaf9e705b963198313c95d0a
BusaidiEssa <essas.work@gmail.com>
2024-09-24 13:41:04 +0400

    Fixing errors 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Box } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/Homepage.jsx"; // Ensure the case matches the component definition
-import CreatePage from "./pages/Createpage.jsx"; // Ensure the case matches the component definition
+import HomePage from "./pages/HomePage.jsx"; // Ensure the case matches the component definition
+import CreatePage from "./pages/CreatePage.jsx"; // Ensure the case matches the component definition
 import Navbar from "./components/Navbar"; // Verify path and component exist
 import { useColorModeValue } from "@chakra-ui/color-mode";
 
