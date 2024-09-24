c8b3166effe114c7c3c20436abe8ebfa0c4df10b
BusaidiEssa <essas.work@gmail.com>
2024-09-24 09:33:29 +0400

    Finishing touches

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,20 @@
-import { Box, Button } from "@chakra-ui/react"
-import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/Homepage.jsx"
-import CreatePage from "./pages/Createpage.jsx"
-import Navbar from "./components/Navbar"
-import { useColorModeValue } from "@chakra-ui/color-mode"
+import { Box } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/Homepage.jsx"; // Verify this matches the exact case and spelling
+import CreatePage from "./pages/Createpage.jsx";
+import Navbar from "./components/Navbar";
+import { useColorModeValue } from "@chakra-ui/color-mode";
 
 function App() {
-
   return (
-    <>
-    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")} > 
-      <Navbar/>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
       </Routes>
     </Box>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
